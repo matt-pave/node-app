a26952581d7c76bdcccf66ff199b9ab1a1dfcda4
Matt <matt@Matts-MacBook-Pro.local>
2021-07-07 13:50:57 +0100

    add npm step

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,4 +1,4 @@
-package _Self.buildTypes
+package NodeApp.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.ScriptBuildStep
@@ -6,32 +6,33 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.dockerCommand
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 
-object Build : BuildType({
+object NodeApp_Build : BuildType({
+    uuid = "4331b12d-ea68-4a69-a209-1459e3441a49"
     name = "Build"
 
     vcs {
-        root(DslContext.settingsRoot)
+        root(NodeApp.vcsRoots.NodeApp_HttpsGithubComMattPaveNodeAppRefsHeadsMain)
     }
-    steps {
-        script {
-            name = "NPM"
-            scriptContent = "npm install"
-            dockerImagePlatform = ScriptBuildStep.ImagePlatform.Linux
-            dockerPull = true
-            dockerImage = "node:current-alpine3.11"
-        }
-        dockerCommand {
-            commandType = push {
-                namesAndTags = "gcr.io/trove-equity/node-app:new"
-            }
-        }
-        dockerCommand {
-            name = "Push"
-            commandType = push {
-                namesAndTags = "gcr.io/trove-equity/node-app:new"
-            }
+steps {
+    script {
+        name = "NPM"
+        scriptContent = "npm install"
+        dockerImagePlatform = ScriptBuildStep.ImagePlatform.Linux
+        dockerPull = true
+        dockerImage = "node:current-alpine3.11"
+    }
+    dockerCommand {
+        commandType = push {
+            namesAndTags = "gcr.io/trove-equity/node-app:new"
         }
     }
+    dockerCommand {
+        name = "Push"
+        commandType = push {
+            namesAndTags = "gcr.io/trove-equity/node-app:new"
+        }
+    }
+}
     triggers {
         vcs {
         }
